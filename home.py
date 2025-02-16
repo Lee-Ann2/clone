00288b5cd3d08d9cdd103b82ae592c2f16992c14
Lee-Ann2 <mfenyanakhazimla17@gmail.com>
2025-02-16 02:21:13 +0200

    Using while loops

--- home.py
+++ home.py
@@ -2,6 +2,11 @@ num_of_players = input('Number of players:\n')
 
 num_of_players = int(num_of_players) # Converting the number from the player to an integer
 
+i = 1
+while i < 108:
+    print(i)
+    i += 1
+
 if num_of_players <= 10:
     print('Hello')
 else:
