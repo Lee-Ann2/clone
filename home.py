42f6b982862843e5617aa7759f077468196be3d0
Lee-Ann2 <mfenyanakhazimla17@gmail.com>
2025-02-16 02:06:19 +0200

    New function in line 5

--- home.py
+++ home.py
@@ -2,5 +2,7 @@ num_of_players = input('Number of players:\n')
 
 num_of_players = int(num_of_players) # Converting the number from the player to an integer
 
-
-
+if num_of_players <= 10:
+    print('Hello')
+else:
+    print('Tf are you all doing in here?')
